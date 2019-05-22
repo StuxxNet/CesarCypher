73635cb798f17e387c3d36363fe47bf0c99e0272
Ramon Borges <ramonboorges@gmail.com>
2019-05-21 23:01:40 -0300

    added a basic try-catch checking to the get_json and save_json_to_file functions

--- cesarcypher.py
+++ cesarcypher.py
@@ -1,23 +1,31 @@
-import requests, json
+import requests, json, sys
 
 def get_json(url):
-    return requests.get(url = url).json()   
-
+    try:
+        return requests.get(url = url).json()
+    except:
+        print("Oooops! Problems on the way.\n{}".format(sys.exc_info()[0]))
+    
 def save_json_to_file(json_returned, file_path):
-    with open(file_path, 'w') as outfile:  
-        json.dump(json_returned, outfile, indent=1)
-    outfile.close()
+    try:
+        with open(file_path, 'w') as outfile:  
+            json.dump(json_returned, outfile, indent=1)
+        outfile.close()
+    except:
+        print("Oooops! Problems on the way.\n{}".format(sys.exc_info()[0]))
     return 0
 
 def decypher_message(msg,overhead):
     print("Overhead: {}\nCyphered Message: {}".format(msg, overhead))
     
-
 if __name__ == "__main__":
     url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=12818de9bd8ad7cba554de3c9e391a3363311a6f"
-    file_path = "return.json"
+    file_path = "answer.json"
     json_returned = get_json(url)
-    if(save_json_to_file(json_returned, file_path) == 0):
-        decypher_message(json_returned["numero_casas"], json_returned["cifrado"])
+    if(isinstance(json_returned,dict)):
+        if(save_json_to_file(json_returned, file_path) == 0):
+            decypher_message(json_returned["numero_casas"], json_returned["cifrado"])
+        else:
+            print("Error while trying to save the JSON file")
     else:
-        print("Error while trying to save the JSON file")
+        print("Oooops! Error while trying to get the JSON")
